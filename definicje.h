7622835e90b8e5908a3542dc992b191036ef9cdb
tomrac96 <152270626+tomrac96@users.noreply.github.com>
2023-11-28 11:26:06 +0100

    Add files via upload

---
+++ definicje.h
@@ -0,0 +1,17 @@
+#ifndef DEFINICJE
+
+#define DEFINICJE
+
+#define SZEROKOSC_POLA 6
+#define WYSOKOSC_POLA 6
+
+typedef enum { PUSTE, BIALY, CZARNY } wielkosc_planszy;
+//https://pl.wikibooks.org/wiki/C/Typy_z%C5%82o%C5%BCone    opis jak działa enum   na tym mozna tez ruchy robic
+
+typedef struct
+{
+    wielkosc_planszy pole[SZEROKOSC_POLA][WYSOKOSC_POLA];
+} plansza;
+
+
+#endif
