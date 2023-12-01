fe3b903850e42964085def09d81d6cdc321cef1a
Lugreer <152274069+Lugreer@users.noreply.github.com>
2023-12-01 16:04:36 +0100

    Update gou.c

--- gou.c
+++ gou.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include <stdio.h>  //to jest nasz plik glowny- dowodzacy... aka Headquarters
 #include "toolsy.h"
 #include "definicje.h"
 
 void main(){
     plansza teatr_wojenny;
+    
 }
