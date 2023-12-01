f4a197b0a2f8c216c357382f69183ee468d49e3a
Lugreer <152274069+Lugreer@users.noreply.github.com>
2023-12-01 16:02:18 +0100

    Update toolsy.h
    🦘

--- toolsy.h
+++ toolsy.h
@@ -1,1 +1,12 @@
+#ifndef TOOLSY
+#define TOOLSY
 
+#include<stdio.h>
+#include "definicje.h"
+
+void wypisz(plansza* teatr_wojenny){
+for(int i=0;i<SZEROKOSC_PLANSZY;i++) //bedzie wypisywal cale wiersze po kolei od gory
+    printf("%d",i);
+}
+
+#endif
