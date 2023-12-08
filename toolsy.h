9c311797a88d85492a382663f0daffda215ab1d7
Lugreer <152274069+Lugreer@users.noreply.github.com>
2023-12-08 14:43:14 +0100

    Update toolsy.h

--- toolsy.h
+++ toolsy.h
@@ -1,36 +1,46 @@
 #ifndef TOOLSY
 #define TOOLSY
-
-#include<stdio.h>
+#include <assert.h>
+#include <malloc.h>
+#include <stdio.h>
 #include "definicje.h"
 
-void wypisz(plansza* teatr_wojenny){
-for(int i=0;i<SZEROKOSC_PLANSZY;i++) //bedzie wypisywal cale wiersze po kolei od gory
-    printf("%d",i);
+plansza sory_memory(plansza *teatr_wojenny){
+    return (plansza) { .ruch_gracza_X=RUCH_GRACZ1};
 }
 
+void vanish(plansza *teatr_wojenny){      //ustawia stan poczatkowy planszy na PUSTY wszystkich miejsc
+    for(int i=0;i<SZEROKOSC_PLANSZY;i++)
+        for(int j=0;j<WYSOKOSC_PLANSZY;j++)
+        teatr_wojenny->plain[i][j]=PUSTE;
+        int kolumna = SZEROKOSC_PLANSZY >> 1, wiersz = WYSOKOSC_PLANSZY >> 1;
+    /*teatr_wojenny->plain[kolumna][wiersz] = teatr_wojenny->plain[kolumna - 1][wiersz - 1] = BIALA;
+    teatr_wojenny->plain[kolumna - 1][wiersz] = teatr_wojenny->plain[kolumna][wiersz - 1] = CZARNA;*/
 
-
+   //return *teatr_wojenny;
+}
 
 void wypisz(plansza *teatr_wojenny)
 {
+    
     printf("  +");
     for(int i = 0; i < 3 * SZEROKOSC_PLANSZY; printf("-"), i++);
     printf("+\n");
+    
     for(int i = 0; i < WYSOKOSC_PLANSZY; i++)
     {
         printf("%2d|", i);
         for(int j = 0; j < SZEROKOSC_PLANSZY; j++)
-            switch(plansza->teatr_wojenny[j][i])
+            switch(teatr_wojenny->plain[j][i])
             {
                 case PUSTE:
-                    printf(" . ");
+                    printf(" + ");
                     break;
                 case BIALA:
-                    printf(" B ");
+                    printf(" O ");
                     break;
                 case CZARNA:
-                    printf(" C ");
+                    printf(" か ");
                     break;
             }
         printf("|\n");
@@ -39,5 +49,7 @@ void wypisz(plansza *teatr_wojenny)
     for(int i = 0; i < 3 * SZEROKOSC_PLANSZY; printf("-"), i++);
     printf("+\n   ");
     for(int i = 0; i < SZEROKOSC_PLANSZY; printf(" %c ", 'A' + i++));
-    printf("\nRuch przypada na %c\n", teatr_wojenny->ruch_gracza_X == RUCH_GRACZ1 ? 'O' : 'X');
+    printf("\nRuch przypada na %c\n", teatr_wojenny->ruch_gracza_X == RUCH_GRACZ1 ? 'C' : 'B');
+}
+
 #endif
