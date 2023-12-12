52166651bf6ea7b03107e2e858239b3f6f5d2095
Lugreer <152274069+Lugreer@users.noreply.github.com>
2023-12-12 11:52:51 +0100

    Update definicje.h

--- definicje.h
+++ definicje.h
@@ -3,9 +3,12 @@
 
 #define SZEROKOSC_PLANSZY 6
 #define WYSOKOSC_PLANSZY 6
+//const int DEFEAT=-(SZEROKOSC_PLANSZY*WYSOKOSC_PLANSZY+100);
+//const int VICTORY=-DEFEAT;
 
+typedef enum { TAK,NIE } koniec;
 typedef enum { PUSTE, BIALA, CZARNA } wielkosc_planszy;
-typedef enum { RUCH_GRACZ1, RUCH_GRACZ2 } kto_sie_rusza;
+typedef enum { RUCH_CZARNY, RUCH_BIALY } kto_sie_rusza;
 //https://pl.wikibooks.org/wiki/C/Typy_z%C5%82o%C5%BCone    opis jak działa enum   na tym mozna tez ruchy robic
 
 typedef struct
@@ -14,5 +17,17 @@ typedef struct
     wielkosc_planszy plain[SZEROKOSC_PLANSZY][WYSOKOSC_PLANSZY];
 } plansza;
 
+typedef struct
+{
+    int wierszyk;
+    int kolumnyk;
+    koniec pas;
+} ruch;
+
+typedef struct element
+{
+    ruch move;
+    struct element* nastepny;
+} rozkaz;
 
 #endif
